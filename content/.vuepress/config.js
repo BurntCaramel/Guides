d1be25afebeb544cd15132ba02f6c024e82351b6
Patrick Smith <patrick@burntcaramel.com>
2019-05-08 23:03:07 +1000

    Improve with tools intro, nav changes

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -3,49 +3,41 @@ module.exports = {
   description: "Guides on creating systems for your team",
   themeConfig: {
     nav: [
-      { text: 'GitHub', link: 'https://github.com/RoyalIcing' }
+      { text: "Blog", link: "/" },
+      { text: "Guides", link: "/guides/" },
+      { text: "Tools", link: "/tools/" },
+      // { text: "GitHub", link: "https://github.com/RoyalIcing" }
     ],
-    sidebar: [
-      // ['/', 'Blog'],
-      {
-        title: 'Guides',
-        collapsable: false,
-        children: [
-          ['/guides/', 'Intro'],
-          ['/guides/fast-web/', 'Fast Web Apps'],
-          ['/guides/web-toolkits/', 'Web Toolkits'],
-          ['/guides/styling-components/', 'Styling Components'],
-          ['/guides/documentation/', 'Writing Documentation'],
-          ['/guides/npm-packages/', 'Making NPM Packages'],
-          ['/guides/deploying-web/', 'Deploying Web Apps & Sites'],
-          ['/guides/writing-backends/', 'Writing Backends'],
-          ['/guides/principles/', 'Principles'],
-          // {
-          //   title: 'Documentation',
-          //   children: [
-          //     ['/guides/documentation/writing', 'Writing Documentation']
-          //   ]
-          // },
-          // ['/guides/components/', 'Components'],
-          // ['/guides/static-sites/', 'Static Sites'],
-        ]
-      },
-      {
-        title: 'Tools',
-        collapsable: false,
-        children: [
-          ['/tools/collectedql/', 'CollectedQL'],
-          ['/tools/datadown/', 'Datadown']
-        ]
-      },
-      // {
-      //   title: 'Conventions',
-      //   collapsable: false,
-      //   children: [
-      //     ['/conventions/schemas/', 'GraphQL Schemas']
-      //   ]
-      // }
-    ],
+    sidebar: {
+      "/guides/": [
+        {
+          title: "Guides",
+          collapsable: false,
+          children: [
+            ["/guides/", "Intro"],
+            ["/guides/fast-web/", "Fast Web Apps"],
+            ["/guides/web-toolkits/", "Web Toolkits"],
+            ["/guides/styling-components/", "Styling Components"],
+            ["/guides/documentation/", "Writing Documentation"],
+            ["/guides/npm-packages/", "Making NPM Packages"],
+            ["/guides/deploying-web/", "Deploying Web Apps & Sites"],
+            ["/guides/writing-backends/", "Writing Backends"],
+          ]
+        }
+      ],
+      "/tools/": [
+        {
+          title: "Tools",
+          collapsable: false,
+          children: [
+            ["/tools/", "Intro"],
+            ["/tools/collectedql/", "CollectedQL"],
+            ["/tools/datadown/", "Datadown"],
+            ["/tools/principles/", "Principles"]
+          ]
+        }
+      ]
+    }
     // displayAllHeaders: true
   }
-}
+};
---
+++ content/tools/README.md
@@ -0,0 +1,9 @@
+# Tools
+
+## [CollectedQL](/tools/collectedql/)
+
+CollectedQL lets you query from content sources using GraphQL. Available content sources include GitHub, Trello, and Figma.
+
+It can be used for prototyping with living content.
+
+Or it can be used in production with Cloudflare’s CDN.
