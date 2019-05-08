4bf4e4411c073540ecbcb9044b91b770509e9afd
Patrick Smith <patrick@burntcaramel.com>
2019-05-08 23:26:19 +1000

    Put under /guides/ base

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,40 +1,28 @@
 module.exports = {
-  // base: "/guides/",
-  title: "Royal Icing",
+  base: "/guides/",
+  title: "Royal Icing · Guides",
   description: "Guides on creating systems for your team",
   themeConfig: {
     nav: [
       { text: "Blog", link: "https://icing.space/" },
-      { text: "Guides", link: "/guides/" },
-      { text: "Tools", link: "/tools/" },
+      { text: "Guides", link: "/" },
+      // { text: "Tools", link: "/tools/" },
       // { text: "GitHub", link: "https://github.com/RoyalIcing" }
     ],
     sidebar: {
-      "/guides/": [
+      "/": [
         {
           title: "Guides",
           collapsable: false,
           children: [
-            ["/guides/", "Intro"],
-            ["/guides/fast-web/", "Fast Web Apps"],
-            ["/guides/web-toolkits/", "Web Toolkits"],
-            ["/guides/styling-components/", "Styling Components"],
-            ["/guides/documentation/", "Writing Documentation"],
-            ["/guides/npm-packages/", "Making NPM Packages"],
-            ["/guides/deploying-web/", "Deploying Web Apps & Sites"],
-            ["/guides/writing-backends/", "Writing Backends"],
-          ]
-        }
-      ],
-      "/tools/": [
-        {
-          title: "Tools",
-          collapsable: false,
-          children: [
-            ["/tools/", "Intro"],
-            ["/tools/collectedql/", "CollectedQL"],
-            ["/tools/datadown/", "Datadown"],
-            ["/tools/principles/", "Principles"]
+            ["/", "Intro"],
+            ["/fast-web/", "Fast Web Apps"],
+            ["/web-toolkits/", "Web Toolkits"],
+            ["/styling-components/", "Styling Components"],
+            ["/documentation/", "Writing Documentation"],
+            ["/npm-packages/", "Making NPM Packages"],
+            ["/deploying-web/", "Deploying Web Apps & Sites"],
+            ["/writing-backends/", "Writing Backends"],
           ]
         }
       ]
