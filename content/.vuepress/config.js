e91e6535cbb8bc596908141cab32b02d17499587
Patrick Smith <patrick@burntcaramel.com>
2019-05-08 23:35:52 +1000

    Revert "Put under /guides/ base"
    This reverts commit 4bf4e4411c073540ecbcb9044b91b770509e9afd.

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,28 +1,40 @@
 module.exports = {
-  base: "/guides/",
-  title: "Royal Icing · Guides",
+  // base: "/guides/",
+  title: "Royal Icing",
   description: "Guides on creating systems for your team",
   themeConfig: {
     nav: [
       { text: "Blog", link: "https://icing.space/" },
-      { text: "Guides", link: "/" },
-      // { text: "Tools", link: "/tools/" },
+      { text: "Guides", link: "/guides/" },
+      { text: "Tools", link: "/tools/" },
       // { text: "GitHub", link: "https://github.com/RoyalIcing" }
     ],
     sidebar: {
-      "/": [
+      "/guides/": [
         {
           title: "Guides",
           collapsable: false,
           children: [
-            ["/", "Intro"],
-            ["/fast-web/", "Fast Web Apps"],
-            ["/web-toolkits/", "Web Toolkits"],
-            ["/styling-components/", "Styling Components"],
-            ["/documentation/", "Writing Documentation"],
-            ["/npm-packages/", "Making NPM Packages"],
-            ["/deploying-web/", "Deploying Web Apps & Sites"],
-            ["/writing-backends/", "Writing Backends"],
+            ["/guides/", "Intro"],
+            ["/guides/fast-web/", "Fast Web Apps"],
+            ["/guides/web-toolkits/", "Web Toolkits"],
+            ["/guides/styling-components/", "Styling Components"],
+            ["/guides/documentation/", "Writing Documentation"],
+            ["/guides/npm-packages/", "Making NPM Packages"],
+            ["/guides/deploying-web/", "Deploying Web Apps & Sites"],
+            ["/guides/writing-backends/", "Writing Backends"],
+          ]
+        }
+      ],
+      "/tools/": [
+        {
+          title: "Tools",
+          collapsable: false,
+          children: [
+            ["/tools/", "Intro"],
+            ["/tools/collectedql/", "CollectedQL"],
+            ["/tools/datadown/", "Datadown"],
+            ["/tools/principles/", "Principles"]
           ]
         }
       ]
